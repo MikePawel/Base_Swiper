f4a402acfec30509c38cceda1d751a319a9070ac
MikePawel <radlinski.mikolaj@gmail.com>
2025-10-17 12:38:42 +0200

    new image

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,8 +4,10 @@ import { twMerge } from "tailwind-merge";
 export const METADATA = {
   name: "Base Swiper — Tinder for Zora",
   description: "Swipe through Zora collectibles and instantly buy them on Base",
-  bannerImageUrl: "https://i.imgur.com/ZIqxpsk.png",
-  iconImageUrl: "https://i.imgur.com/rLshpa8.png",
+  bannerImageUrl:
+    "https://raw.githubusercontent.com/MikePawel/Base_Swiper/refs/heads/main/public/banner.png",
+  iconImageUrl:
+    "https://raw.githubusercontent.com/MikePawel/Base_Swiper/refs/heads/main/public/icon.png",
   homeUrl: process.env.NEXT_PUBLIC_URL ?? "https://base-swiper.vercel.app",
   splashBackgroundColor: "#FFFFFF",
 };
